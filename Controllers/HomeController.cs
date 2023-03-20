222ef53ed647322cfee8e838522d847e19c0f60d
A.J. Marchese <aj.marchese@outlook.com>
2023-03-20 09:03:43 -0700

    added comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,22 @@
-﻿using Google.Apis.Books.v1.Data;
-using GoogleBooksApp.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿using Google.Apis.Books.v1.Data; // Import the Google Books API data models
+using GoogleBooksApp.Services; // Import the Google Books API client service
+using Microsoft.AspNetCore.Mvc; // Import the ASP.NET Core MVC library
 
 namespace GoogleBooksApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller // Define a controller class named HomeController
     {
-        private readonly GoogleBooksApiClient googleBooksApiClient;
+        private readonly GoogleBooksApiClient googleBooksApiClient; // Declare a field to hold an instance of the GoogleBooksApiClient class
 
-        public HomeController()
+        public HomeController() // Define a constructor method for the HomeController class
         {
-            googleBooksApiClient = new GoogleBooksApiClient();
+            googleBooksApiClient = new GoogleBooksApiClient(); // Create a new instance of the GoogleBooksApiClient class and assign it to the googleBooksApiClient field
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index() // Define a method named Index that returns an IActionResult
         {
-            var books = await googleBooksApiClient.GetBooksAsync("subject:fiction");
-            return View(books.Items);
+            var books = await googleBooksApiClient.GetBooksAsync("subject:fiction"); // Call the GetBooksAsync method of the GoogleBooksApiClient instance to get a list of books
+            return View(books.Items); // Pass the list of books to the View method and return the result as an IActionResult
         }
     }
 }
--- Services/GoogleBooksApiClient.cs
+++ Services/GoogleBooksApiClient.cs
@@ -7,9 +7,13 @@ namespace GoogleBooksApp.Services
 {
     public class GoogleBooksApiClient
     {
+        // The API key used for accessing the Google Books API
         private readonly string apiKey = "AIzaSyCIv33otYHsBVeQSgEE3_5SysFKQcGCIfQ";
+
+        // A BooksService object used for making requests to the API
         private readonly BooksService booksService;
 
+        // Constructor that initializes the BooksService object
         public GoogleBooksApiClient()
         {
             booksService = new BooksService(new BaseClientService.Initializer
@@ -19,10 +23,16 @@ namespace GoogleBooksApp.Services
             });
         }
 
+        // Method that retrieves a list of books from the Google Books API based on a query
         public async Task<Volumes> GetBooksAsync(string query, int maxResults = 40)
         {
+            // Create a request to search for volumes (books) with the specified query
             var request = booksService.Volumes.List(query);
+
+            // Set the maximum number of results to be returned
             request.MaxResults = maxResults;
+
+            // Execute the request and return the results
             return await request.ExecuteAsync();
         }
     }
