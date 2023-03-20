6a8b6ba4c4d222c9133d5cc7f1fca21a1b0620cf
A.J. Marchese <aj.marchese@outlook.com>
2023-03-20 08:55:06 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Google.Apis.Books.v1.Data;
+using GoogleBooksApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleBooksApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly GoogleBooksApiClient googleBooksApiClient;
+
+        public HomeController()
+        {
+            googleBooksApiClient = new GoogleBooksApiClient();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await googleBooksApiClient.GetBooksAsync("subject:fiction");
+            return View(books.Items);
+        }
+    }
+}
---
+++ Services/GoogleBooksApiClient.cs
@@ -0,0 +1,29 @@
+﻿using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+using Google.Apis.Services;
+using System.Threading.Tasks;
+
+namespace GoogleBooksApp.Services
+{
+    public class GoogleBooksApiClient
+    {
+        private readonly string apiKey = "AIzaSyCIv33otYHsBVeQSgEE3_5SysFKQcGCIfQ";
+        private readonly BooksService booksService;
+
+        public GoogleBooksApiClient()
+        {
+            booksService = new BooksService(new BaseClientService.Initializer
+            {
+                ApiKey = apiKey,
+                ApplicationName = "GoogleBooksApp",
+            });
+        }
+
+        public async Task<Volumes> GetBooksAsync(string query, int maxResults = 40)
+        {
+            var request = booksService.Volumes.List(query);
+            request.MaxResults = maxResults;
+            return await request.ExecuteAsync();
+        }
+    }
+}
