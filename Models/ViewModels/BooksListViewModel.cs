e8f60cf277d4431b08513f226b45cea743a27c45
A.J. Marchese <aj.marchese@outlook.com>
2023-04-24 09:00:20 -0700

    added view model for books

---
+++ Models/ViewModels/BooksListViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace YourAppNamespace.Models.ViewModels
+{
+    public class BookListViewModel
+    {
+        public IEnumerable<GoogleBook> Books { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+
+    public class GoogleBook
+    {
+        public string Id { get; set; }
+        public VolumeInfo VolumeInfo { get; set; }
+    }
+
+    public class VolumeInfo
+    {
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public string Description { get; set; }
+        public string PublishedDate { get; set; }
+        public ImageLinks ImageLinks { get; set; }
+    }
+
+    public class ImageLinks
+    {
+        public string Thumbnail { get; set; }
+    }
+
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int)System.Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
