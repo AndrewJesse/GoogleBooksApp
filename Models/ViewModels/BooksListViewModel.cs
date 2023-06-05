a3a7573e23a5d1836abe40fd654f1e46dec00c3b
Andrew J Marchese <aj.marchese@outlook.com>
2023-06-04 20:48:11 -0700

    addressed warnings to avoid nullable references

--- Models/ViewModels/BooksListViewModel.cs
+++ Models/ViewModels/BooksListViewModel.cs
@@ -6,28 +6,28 @@ namespace YourAppNamespace.Models.ViewModels
 {
     public class BookListViewModel
     {
-        public IEnumerable<GoogleBook> Books { get; set; }
-        public PagingInfo PagingInfo { get; set; }
+        public IEnumerable<GoogleBook>? Books { get; set; }
+        public PagingInfo? PagingInfo { get; set; }
     }
 
     public class GoogleBook
     {
-        public string Id { get; set; }
-        public VolumeInfo VolumeInfo { get; set; }
+        public string? Id { get; set; }
+        public VolumeInfo? VolumeInfo { get; set; }
     }
 
     public class VolumeInfo
     {
-        public string Title { get; set; }
-        public List<string> Authors { get; set; }
-        public string Description { get; set; }
-        public string PublishedDate { get; set; }
-        public ImageLinks ImageLinks { get; set; }
+        public string? Title { get; set; }
+        public List<string>? Authors { get; set; }
+        public string? Description { get; set; }
+        public string? PublishedDate { get; set; }
+        public ImageLinks? ImageLinks { get; set; }
     }
 
     public class ImageLinks
     {
-        public string Thumbnail { get; set; }
+        public string? Thumbnail { get; set; }
     }
 
     public class PagingInfo
