a574fefef7a3eacd7fe70d3676079e085517a337
A.J. Marchese <aj.marchese@outlook.com>
2023-06-24 10:06:10 -0700

    added _ViewLayout file to Pages

---
+++ Pages/_ViewLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoogleBooksApp.Pages
+{
+    public class _ViewLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
