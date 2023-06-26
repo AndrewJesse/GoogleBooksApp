6e0c5a0a11269ba8d9d2e220a82d25760bdfb5fd
A.J. Marchese <aj.marchese@outlook.com>
2023-06-26 08:27:36 -0700

    added editor

--- Pages/Users/Editor.cshtml.cs
+++ Pages/Users/Editor.cshtml.cs
@@ -1,12 +1,69 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace GoogleBooksApp.Pages.Users
 {
-    public class EditorModel : PageModel
+    public class EditorModel : AdminPageModel
     {
-        public void OnGet()
+        public UserManager<IdentityUser> UserManager;
+
+        public EditorModel(UserManager<IdentityUser> usrManager)
         {
+            UserManager = usrManager;
+        }
+
+        [BindProperty]
+        public string Id { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string UserName { get; set; } = string.Empty;
+
+        [BindProperty]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [BindProperty]
+        public string? Password { get; set; }
+
+        public async Task OnGetAsync(string id)
+        {
+            IdentityUser user = await UserManager.FindByIdAsync(id);
+            Id = user.Id;
+            UserName = user.UserName;
+            Email = user.Email;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await UserManager.FindByIdAsync(Id);
+                user.UserName = UserName;
+                user.Email = Email;
+
+                IdentityResult result = await UserManager.UpdateAsync(user);
+
+                if (result.Succeeded && !string.IsNullOrEmpty(Password))
+                {
+                    await UserManager.RemovePasswordAsync(user);
+                    result = await UserManager.AddPasswordAsync(user, Password);
+                }
+
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("List");
+                }
+
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+
+            return Page();
         }
     }
 }
