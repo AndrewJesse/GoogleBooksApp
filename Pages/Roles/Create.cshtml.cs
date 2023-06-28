03d70e465a2e512433e660fa7281cd92601b9054
A.J. Marchese <aj.marchese@outlook.com>
2023-06-28 08:19:05 -0700

    establishes create Raozr Page

---
+++ Pages/Roles/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoogleBooksApp.Pages.Roles
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
