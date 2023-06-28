2959d8e8fbe365b1d248b9f7c6e25418220692f1
A.J. Marchese <aj.marchese@outlook.com>
2023-06-28 08:28:46 -0700

    moved code to code behind

--- Pages/Roles/Create.cshtml.cs
+++ Pages/Roles/Create.cshtml.cs
@@ -1,12 +1,37 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace GoogleBooksApp.Pages.Roles
 {
-    public class CreateModel : PageModel
+    public class CreateModel : AdminPageModel
     {
-        public void OnGet()
+        public RoleManager<IdentityRole> RoleManager;
+
+        public CreateModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
+            RoleManager = roleManager;
+        }
+
+        [BindProperty]
+        public string Name { get; set; } = string.Empty;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole role = new IdentityRole { Name = Name };
+                IdentityResult result = await RoleManager.CreateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("List");
+                }
+                foreach (IdentityError err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+            }
+            return Page();
         }
     }
 }
