b10a3376b74c3cc2df81ca932422d80c8a950c9d
A.J. Marchese <aj.marchese@outlook.com>
2023-06-30 08:28:01 -0700

    fixed datacontext code

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -3,17 +3,14 @@ using System;
 
 namespace GoogleBooksApp.Models
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
-        public class DataContext : DbContext
-        {
 
-            public DataContext(DbContextOptions<DataContext> opts)
-            : base(opts) { }
+        public DataContext(DbContextOptions<DataContext> opts)
+        : base(opts) { }
 
-            public DbSet<Person> People => Set<Person>();
-            public DbSet<Department> Departments => Set<Department>();
-            public DbSet<Location> Locations => Set<Location>();
-        }
+        public DbSet<Person> People => Set<Person>();
+        public DbSet<Department> Departments => Set<Department>();
+        public DbSet<Location> Locations => Set<Location>();
     }
 }
