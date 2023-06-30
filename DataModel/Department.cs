6d81b546f45b950cc65693f6172c24c806ca9503
A.J. Marchese <aj.marchese@outlook.com>
2023-06-30 08:56:36 -0700

    moved the datamodel to the project

---
+++ DataModel/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleBooksApp
+{
+    public class Department
+    {
+
+        public long Departmentid { get; set; }
+        public string Name { get; set; } = String.Empty;
+
+        public IEnumerable<Person>? People { get; set; }
+    }
+}
---
+++ DataModel/Location.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleBooksApp
+{
+    public class Location
+    {
+
+        public long LocationId { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = String.Empty;
+
+        public IEnumerable<Person>? People { get; set; }
+    }
+}
