08ae47861e3855b808629a0149c33280167bca06
A.J. Marchese <aj.marchese@outlook.com>
2023-07-04 09:45:26 -0700

    think i got it

--- Models/User.cs
+++ Models/User.cs
@@ -3,9 +3,9 @@ namespace GoogleBooksApp.Models
 {
     public class User : IdentityUser
     {
-        public int? UserId { get; set; }
-        public string? Username { get; set; }
-        public string? Password { get; set; } //Note: Always hash and salt your passwords
+        //public int? UserId { get; set; }
+        //public string? Username { get; set; }
+        //public string? Password { get; set; } //Note: Always hash and salt your passwords
 
         // Navigation property
         public ICollection<FavoriteBook>? FavoriteBooks { get; set; }
