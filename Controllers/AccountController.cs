2fe6578cd4f7d7701934c4f63fdd779a812bc155
A.J. Marchese <aj.marchese@outlook.com>
2023-07-04 09:28:26 -0700

    added contents of controller file

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,45 @@
-﻿namespace GoogleBooksApp.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using GoogleBooksApp.Models;
+using System.Threading.Tasks;
+
+namespace GoogleBooksApp.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            var user = await userManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
